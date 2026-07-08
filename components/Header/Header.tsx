221b876cb42b17842c097ca94882f9c9e18dabe0
Aleksandr Zykov <Finnimonius97@gmail.com>
2026-07-08 17:59:17 +0400

    feat: create dropdown component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,22 @@
 import Link from "next/link";
 import styles from "./Header.module.scss";
 import Image from "next/image";
+import DropdownMenu from "../ui/Dropdown/DropdownMenu";
+
+const items = [
+  {
+    label: "Пункт меню",
+    key: "1",
+  },
+  {
+    label: "Пункт меню",
+    key: "2",
+  },
+  {
+    label: "Пункт меню",
+    key: "3",
+  },
+];
 
 export default function Header() {
   return (
@@ -10,6 +26,8 @@ export default function Header() {
           <Link href="/">
             <Image src="/images/logo.png" alt="Логотип Starvell" width={118} height={24}/>
           </Link>
+          <input type="text" placeholder="Поиск игр и приложений"/>
+          <DropdownMenu text="Поддержка" items={items}/>
         </div>
       </div>
     </header>
---
+++ components/ui/Dropdown/DropdownMenu.tsx
@@ -0,0 +1,51 @@
+"use client";
+import type { MenuProps } from "antd";
+import { Dropdown } from "antd";
+import { useState } from "react";
+import styles from "./DropdownMenu.module.scss";
+
+interface Props {
+  text: string;
+  items: MenuProps["items"];
+}
+
+export default function DropdownMenu({ text, items }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick: MenuProps["onClick"] = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Dropdown 
+        menu={{ items, onClick, className: styles.dropdownList }}
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        trigger={['hover']} 
+      >
+        <a onClick={(e) => e.preventDefault()} className={styles.trigger}>
+          <span className={`${styles.text} ${isOpen ? styles.textOpen : ''}`}>
+            {text}
+          </span>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={`${styles.arrow} ${isOpen ? styles.arrowOpen : ''}`}
+          >
+            <path
+              d="M5.11989 6.56006L7.99989 9.44006L10.8799 6.56006"
+              stroke="#666D80"
+              strokeWidth="1.2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </a>
+      </Dropdown>
+    </>
+  );
+}
